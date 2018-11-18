420cf12c3ab821528f46ca431be22de99e998f3c
Puff cat <1042229484@qq.com>
2018-11-18 09:35:28 +0800

    完成首页的柱状图和饼图

---
+++ public/back2/js/isLogin.js
@@ -0,0 +1,15 @@
+$.ajax({
+  type: "get",
+  url: "/employee/checkRootLogin",
+  dataType: "json",
+  success: function( info ) {
+    console.log( info );
+    if ( info.success ) {
+      console.log("用户已登录,继续访问");
+    }
+    if ( info.error === 400 ) {
+      // 未登录
+      location.href = "login.html";
+    }
+  }
+})
